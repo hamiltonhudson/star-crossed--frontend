af645944ea778b476bb6d9bc8602886d3debf0ad
Hamilton Hudson <ehamiltonhudson@gmail.com>
2019-01-31 23:07:31 -0500

    successful fetching of users and sun in App

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 class App extends Component {
 
-  // componentDidMount() {
-  //   fetch(`http://localhost:3000/api/v1/users`)
-  //     .then(r => console.log(r.json))
-  // }
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/suns')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={'../images/astro_cal.jpg'}className="App-logo" alt="astrology-calendar" />
-          <a
+          <h1 className="App-title">App.js</h1>
+          <img src={'../images/astro_cal.jpg'} className="App-image" alt="astrology-calendar" />
+          {/* <a
             className="App-link" href="https://reactjs.org"
             target="_blank" rel="noopener noreferrer"
-          >
+            >
             react link
-          </a>
+          </a> */}
         </header>
       </div>
     );
